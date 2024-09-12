12a11658877a96ca710ef1d9cb33e051d65ed1b8
joestaxk <joestaxk@gmail.com>
2024-09-12 06:52:53 -0400

    added: some few changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Stats from "./pages/stats.jsx";
 
